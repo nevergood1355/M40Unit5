fec4c939b0f3e86d3a433721d8d7d41763e80cb3
nevergood1355 <41524115+nevergood1355@users.noreply.github.com>
2021-01-24 23:08:32 +0400

    MyFragment now implements ScreenView interface

--- app/src/main/java/com/skill_factory/unit5/MyFragment.kt
+++ app/src/main/java/com/skill_factory/unit5/MyFragment.kt
@@ -10,37 +10,41 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import javax.inject.Inject
 
-class MyFragment() : Fragment() {
+class MyFragment() : Fragment(), ScreenView {
     @Inject
     lateinit var screen1Presenter: Screen1Presenter
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.screen, container, false)
     }
 
     override fun onResume() {
         super.onResume()
-        screen1Presenter.attachView(object : ScreenView{
-            override fun setIcon(idIcon: Int) {
-                requireView().findViewById<ImageView>(R.id.icon).setImageResource(idIcon)
-            }
-
-            override fun setPosts(text: String) {
-                requireView().findViewById<TextView>(R.id.text_posts).text = text
-            }
-
-            override fun setFollowers(text: String) {
-                requireView().findViewById<TextView>(R.id.text_followers).text = text
-            }
-
-            override fun setFollowing(text: String) {
-                requireView().findViewById<TextView>(R.id.text_following).text = text
-            }
-        })
+        screen1Presenter.attachView(this)
     }
 
     override fun onPause() {
         super.onPause()
         screen1Presenter.detach()
+    }
+
+    override fun setIcon(idIcon: Int) {
+        requireView().findViewById<ImageView>(R.id.icon).setImageResource(idIcon)
+    }
+
+    override fun setPosts(text: String) {
+        requireView().findViewById<TextView>(R.id.text_posts).text = text
+    }
+
+    override fun setFollowers(text: String) {
+        requireView().findViewById<TextView>(R.id.text_followers).text = text
+    }
+
+    override fun setFollowing(text: String) {
+        requireView().findViewById<TextView>(R.id.text_following).text = text
     }
 }
